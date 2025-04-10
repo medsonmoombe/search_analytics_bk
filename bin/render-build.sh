46841ac21fc0b8a4a2ed389cccb080d8ed703e8d
medsonmoombe <medsonmoombe21@gmail.com>
2025-04-10 23:34:40 +0200

    Fixed port issue

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,13 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails db:migrate
 
+echo "-----> Ensuring port $PORT is free"
+lsof -ti:$PORT | xargs kill -9 || true
+
+echo "-----> Creating restart file"
+mkdir -p tmp
+touch tmp/restart.txt
+
 # Make sure all scripts are executable
 chmod +x bin/rails
 chmod +x bin/render-build.sh
