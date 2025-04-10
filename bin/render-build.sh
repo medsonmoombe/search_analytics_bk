2cceb582f59706dd1409a11fb5974a83b87f9588
medsonmoombe <medsonmoombe21@gmail.com>
2025-04-10 19:08:13 +0200

    update script to be executable

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,3 +4,8 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails db:migrate
+
+# Make sure all scripts are executable
+chmod +x bin/rails
+chmod +x bin/render-build.sh
+chmod +x bin/bundle
